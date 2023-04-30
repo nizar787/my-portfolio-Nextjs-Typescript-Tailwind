c8bb01af96436071045c3a93e0c2f65c1e1df501
nizar787 <nizarharb7@gmail.com>
2023-04-30 17:47:50 +0100

    fix y-scrollbar in experience page

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -3,22 +3,25 @@ import { motion } from 'framer-motion'
 import ExperienceCard from './ExperienceCard'
 import { Experience } from '../typings'
 type Props = {
-  experiences:Experience[]
+  experiences: Experience[]
 }
 
-function WorkExperience({experiences}: Props) {
+function WorkExperience({ experiences }: Props) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='h-screen flex relative overflow-y-scroll flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
+      // overflow-y-scroll
+      className='h-screen flex relative  flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
     >
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-1xl'>
         Experience
       </h3>
       <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
-        {experiences?.map(experience=>( <ExperienceCard key={experience._id} experience={experience} />))}
+        {experiences?.map((experience) => (
+          <ExperienceCard key={experience._id} experience={experience} />
+        ))}
       </div>
     </motion.div>
   )
